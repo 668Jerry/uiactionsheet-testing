d3fde648976c8be9cef5ae0c19a617efed7f774f
Novel <668Jerry@gmail.com>
2015-06-02 07:29:11 +0800

    init mainviewcontroller

--- uiaction sheet test/AppDelegate.m
+++ uiaction sheet test/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "MainViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,11 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc]initWithFrame:[[UIScreen mainScreen]bounds]];
+    [self.window setBackgroundColor:[UIColor greenColor]];
+    MainViewController *mainViewController = [[MainViewController alloc]init];
+    [self.window addSubview:mainViewController.view];
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
---
+++ uiaction sheet test/view/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  uiaction sheet test
+//
+//  Created by lololol on 2/Jun/15.
+//  Copyright (c) 2015 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
